ab1f60b7c22dcff4344cf9ba2c0a43a0910d4c7f
remeh <remeh@remeh.fr>
2017-01-24 13:54:37 +0100

    checkout: start checkout implementation.

---
+++ src/checkout.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class Checkout extends Component {
+  constructor(props) {
+    super(props);
+
+    window.Stripe.setPublishableKey('pk_test_4Ukbv9lXi2SBW71FbTYsVyiK');
+
+    this.state = {
+      open: props.open,
+      disabledSubmit: false,
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      open: nextProps.open,
+    })
+  }
+  
+  close = () => {
+    this.props.checkoutClose();
+  }
+
+  checkout = () => {
+    this.setState({
+      disabledSubmit: true,
+    });
+
+    // TODO(remy): gather data to send it to Stripe
+    let c = {
+      number: 4242424242424242,
+      cvc: 555,
+      exp_month: 12,
+      exp_year: 19,
+      address_zip: 69100,
+    };
+
+    window.Stripe.card.createToken(c, this.stripeResponseHandler);
+  }
+
+  stripeResponseHandler = (status, response) => {
+    // TODO(remy): implement me.
+    console.log(status);
+    console.log(response);
+  }
+
+  render() {
+    return <Dialog
+             title="One year subscription"
+             modal={false}
+             open={this.state.open}
+             onRequestClose={this.props.onClose}>
+             <form action="/your-charge-code" method="POST" id="payment-form">
+              <span className="payment-errors"></span>
+
+              <div className="form-row">
+                <label>
+                  <span>Card Number</span>
+                  <input type="text" size="20" data-stripe="number" />
+                </label>
+              </div>
+
+              <div className="form-row">
+                <label>
+                  <span>Expiration (MM/YY)</span>
+                  <input type="text" size="2" data-stripe="exp_month" />
+                </label>
+                <span> / </span>
+                <input type="text" size="2" data-stripe="exp_year" />
+              </div>
+
+              <div className="form-row">
+                <label>
+                  <span>CVC</span>
+                  <input type="text" size="4" data-stripe="cvc" />
+                </label>
+              </div>
+
+              <div className="form-row">
+                <label>
+                  <span>Billing Postal Code</span>
+                  <input type="text" size="6" data-stripe="address_zip" />
+                </label>
+              </div>
+
+              <RaisedButton disabled={this.state.disabledSubmit} onClick={this.checkout} label="Submit Payment" fullWidth={true} />
+            </form>
+          </Dialog>
+  }
+}
+
+export default Checkout;
--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 
 import Create from 'material-ui/svg-icons/content/create';
+import CreditCard from 'material-ui/svg-icons/action/credit-card';
 import Drawer from 'material-ui/Drawer';
 import Divider from 'material-ui/Divider';
 import Forum from 'material-ui/svg-icons/communication/forum';
@@ -9,6 +10,7 @@ import MenuItem from 'material-ui/MenuItem';
 import PowerSettingsNew from 'material-ui/svg-icons/action/power-settings-new';
 import Subheader from 'material-ui/Subheader';
 
+import Checkout from './checkout.js';
 import XHRAccount from './xhr/account.js';
 
 class Menu extends Component {
@@ -16,6 +18,7 @@ class Menu extends Component {
     super(props);
     this.state = {
       open: props.open,
+      checkout: false,
     }
   }
 
@@ -46,6 +49,15 @@ class Menu extends Component {
     this.props.onMemo();
   }
 
+  openCheckout = () => {
+    this.setState({ checkout: true });
+    this.props.toggleMenu();
+  }
+
+  closeCheckout = () => {
+    this.setState({ checkout: false });
+  }
+
   // ----------------------
 
   render() {
@@ -57,8 +69,10 @@ class Menu extends Component {
           <Divider />
           <MenuItem primaryText="Write a memo" onClick={this.memo} leftIcon={<Create />} />
           <Divider />
+          <MenuItem primaryText="Checkout" onClick={this.openCheckout} leftIcon={<CreditCard />} />
           <MenuItem primaryText="Support" onClick={this.support} leftIcon={<Forum />} />
           <MenuItem primaryText="Logout" onClick={this.logout} leftIcon={<PowerSettingsNew />} />
+          <Checkout open={this.state.checkout} onClose={this.closeCheckout} />
       </Drawer>
   }
 }
