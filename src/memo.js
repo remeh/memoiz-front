d8b9713cecb3f1dc692e0df0d307f1e1d80a4018
remeh <remeh@remeh.fr>
2017-02-08 21:04:39 +0100

    memo: cut down the size of a memo.

--- src/memo.js
+++ src/memo.js
@@ -173,28 +173,29 @@ class Memo extends Component {
         onDragLeave={this.onDragLeave}
         onDrop={this.onDrop}
         >
+
         <div onClick={this.onClick} className={this.computeContentClass()}>
           <div>
-
             <p>{this.getText()}</p>
             {this.state.r_category !== 'Uncategorized' &&
               <Chip text={this.state.r_category} />
             }
           </div>
         </div>
-          { this.props.memo.r_image &&
-            <div className="rich">
-              <a href={this.props.memo.r_url} target="_blank" alt="Go to link">
-              <div className="desc">
-                <p className="title">{this.props.memo.r_title}</p>
-                <p className="url">{this.props.memo.r_url}</p>
-              </div>
-              <div>
-                  <img src={this.props.memo.r_image} role="presentation" />
-              </div>
-              </a>
+        {this.props.memo.r_image &&
+          <div className="rich">
+            <a href={this.props.memo.r_url} target="_blank" alt="Go to link">
+            <div className="desc">
+              <p className="title">{this.props.memo.r_title}</p>
+              <p className="url">{this.props.memo.r_url}</p>
             </div>
-          }
+            <div>
+                <img src={this.props.memo.r_image} role="presentation" />
+            </div>
+            </a>
+          </div>
+        }
+
       </div>
     );
   }
