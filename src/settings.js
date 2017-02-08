e0b750187be78c51ec397780542d1f80145b2755
remeh <remeh@remeh.fr>
2017-02-08 13:14:04 +0100

    settings: add button to go to the plan.

--- src/settings.js
+++ src/settings.js
@@ -4,6 +4,7 @@ import Moment from 'react-moment';
 import AppBar from 'material-ui/AppBar';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import Helpers from './helpers.js';
 import { Menu, MenuModes } from './menu.js';
@@ -83,6 +84,9 @@ class Settings extends Component {
             <h3>Subscription</h3>
             {this.state.subscribed && <h4><em>{this.state.plan.name} plan</em> valid until <strong><Moment format='LLL'>{this.state.planValidUntil}</Moment></strong></h4>}
             {!this.state.subscribed && this.state.trial && <h4>Free trial until <strong><Moment format='LLL'>{this.state.trialValidUntil}</Moment></strong></h4>}
+            {!this.state.subscribed && !this.state.trial && <div><h4>Your free trial is over.</h4>
+            <RaisedButton primary={true} onClick={this.gotoCheckout} label="discover our plans"></RaisedButton>
+            </div>}
             <h3>Delete my account</h3>
             <h4>Contact us at <a href='mailto:support@memoiz.com'>contact@memoiz.com</a> to delete your account.</h4>
           </div>
@@ -92,4 +96,3 @@ class Settings extends Component {
 }
 
 export default Settings;
-
